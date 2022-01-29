cff03d534e469dfee316e6275f7ef41a1e3d3409
yumimm <yuu9916@gmail.com>
2022-01-29 23:46:11 +0800

    feat : Add Signin path

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import NotFound from '../views/NotFound.vue'
+import SignIn from '../views/SignIn.vue'
 
 Vue.use(VueRouter)
 // 路由清單
 const routes = [
+  {
+    path: '/signin',
+    name: 'sing-in',
+    component: SignIn
+  },
   {
     path: '*',
     name: 'not-found',
