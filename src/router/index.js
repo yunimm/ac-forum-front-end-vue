93195264983e391b1f8e41011b464499180363f0
yumimm <yuu9916@gmail.com>
2022-02-05 02:30:01 +0800

    feat: 新增路由設定

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,43 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import NotFound from '../views/NotFound.vue'
 import SignIn from '../views/SignIn.vue'
+import Restaurants from '../views/Restaurants.vue'
 
 Vue.use(VueRouter)
 // 路由清單
 const routes = [
   {
+    path:'/',
+    name:'roots',
+    redirect: '/restaurants'
+  },
+  {
     path: '/signin',
     name: 'sing-in',
     component: SignIn
   },
+  {
+    path: '/signup',
+    name: 'sign-up',
+    component: () => import('../views/SignUp.vue')
+  },
+  { path: '/restaurants',
+    name: 'restaurants',
+    component: Restaurants
+  },
+  { path: '/restaurants/feeds',
+    name: 'restaurants-feeds',
+    component: () => import('../views/RestaurantsFeeds.vue')
+  },
+  { path: '/restaurants/top',
+    name: 'restaurants-tops',
+    component: () => import('../views/RestaurantsTop.vue')
+  },
+  { path: '/users/top',
+    name: 'users-top',
+    component: () => import('../views/UsersTop.vue')
+  },
+  
   {
     path: '*',
     name: 'not-found',
@@ -19,6 +47,7 @@ const routes = [
 ]
 
 const router = new VueRouter({
+  linkExactActiveClass: 'active', 
   routes
 })
 
