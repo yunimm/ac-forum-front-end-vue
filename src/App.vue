22edb2d429430f25fe43f2552427e1f3cb0c4fa4
yumimm <yuu9916@gmail.com>
2022-02-05 02:31:20 +0800

    feat: 載入navbar components

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div id="restaurant-forum">
+    <Navbar />
+    <main role="main" class="mt-5">
     <router-view/>
+    </main>
   </div>
 </template>
 
 // 載入bootstrap套件
+// 載入navbar
 <script>
 import 'bootstrap/dist/css/bootstrap.min.css' 
 import 'bootstrap'
+import Navbar from './components/Navbar.vue'
 export default {
-  name: 'App',
+  components: {
+    Navbar
+  }
 }
 </script>
