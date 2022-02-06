c046cc08f08a2abb85c1424d1bf438a569533128
yumimm <yuu9916@gmail.com>
2022-02-06 18:51:49 +0800

    feat: add router

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -36,7 +36,7 @@
         <!-- is user is login -->
         <template v-if="isAuthenticated" >
           <router-link
-             to="#" 
+             :to="{ name: 'user', params: { id: currentUser.id } }" 
              class="text-white mr-3"
           > 
            {{ currentUser.name || '使用者' }} 您好
@@ -82,7 +82,7 @@ export default {
   },
   methods: {
     // 會向後端拉取API資料
-    fetchUser() {
+    fetchUser () {
       this.currentUser = {
         ...this.currentUser,
         ...dummyUser.currentUser
