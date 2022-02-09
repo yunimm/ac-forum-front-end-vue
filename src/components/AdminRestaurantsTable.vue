a02b3015e148a26ee9770a82ecc638d6e2f17a5d
yumimm <yuu9916@gmail.com>
2022-02-09 23:59:52 +0800

    feat: add router-link, add "deleteRestaurant" mehtods

--- src/components/AdminRestaurantsTable.vue
+++ src/components/AdminRestaurantsTable.vue
@@ -18,9 +18,11 @@
         <td class="d-flex justify-content-between">
           <router-link :to="{ name: 'admin-restaurant', params: { id: restaurant.id }}" class="btn btn-link">Show</router-link>
 
-          <a href="#" class="btn btn-link">Edit</a>
+          <router-link :to="{ name: 'admin-restaurant-edit', params: { id:restaurant.id }}" class="btn btn-link">Edit</router-link>
 
-          <button type="button" class="btn btn-link">Delete</button>
+          <button
+          @click.stop.prevent="deleteRestaurant(restaurant.id)"
+          type="button" class="btn btn-link">Delete</button>
         </td>
       </tr>
     </tbody>
@@ -996,7 +998,12 @@ export default {
   methods: {
     fetchRestaurants () {
       this.restaurants = dummyData.restaurants
+    },
+    deleteRestaurant (restaurantId) {
+      this.restaurants = this.restaurants.filter
+      (restaurant => restaurant.id !== restaurantId)
     }
+    
   }
 
 }
