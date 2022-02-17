8fd70ffee7357a32e7907d9bfe100222ab284d2a
yumimm <yuu9916@gmail.com>
2022-02-18 00:48:35 +0800

    init

---
+++ src/apis/user.js
@@ -0,0 +1,40 @@
+import { apiHelper } from "./../utils/helpers";
+const getToken = () => localStorage.getItem("token");
+
+export default {
+  addFavorite({restaurantId}) {
+    return apiHelper.post(`/favorite/${restaurantId}`, null, {
+      headers: { Authorization: `Bearer ${getToken()}` },
+    });
+  },
+  deleteFavorite({restaurantId}) {
+    return apiHelper.delete(`/favorite/${restaurantId}`, {
+      headers: { Authorization: `Bearer ${getToken()}` },
+    });
+  },
+  addLike({restaurantId}) {
+    return apiHelper.post(`/like/${restaurantId}`,null,{
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
+  },
+  deleteLike({restaurantId}) {
+    return apiHelper.delete(`/like/${restaurantId}`,{
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
+  },
+  getTopUsers() {
+    return apiHelper.get('/users/top', {
+      headers: { Authorization: `Bearer ${getToken()}`}
+    })
+  },
+  addFollowing({userId}) {
+    return apiHelper.post(`/following/${userId}`,null,{
+      headers: {  Authorization: `Bearer ${getToken()}`}
+    })
+  },
+  deleteFollowing({userId}) {
+    return apiHelper.delete(`/following/${userId}`,{
+      headers: {  Authorization: `Bearer ${getToken()}`}
+    })
+  }
+};
