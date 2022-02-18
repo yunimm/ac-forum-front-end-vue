046d64bc6184d426695047f632bf6ca1ab7aca30
yumimm <yuu9916@gmail.com>
2022-02-19 00:40:39 +0800

    feat: connect API

--- src/views/AdminRestaurantNew.vue
+++ src/views/AdminRestaurantNew.vue
@@ -6,19 +6,36 @@
 </template>
 
 <script>
-import AdminRestaurantForm from "./../components/AdminRestaurantForm.vue"
-
+import AdminRestaurantForm from "./../components/AdminRestaurantForm.vue";
+import adminAPI from "./../apis/admin";
+import { Toast } from "./../utils/helpers";
 
 export default {
   components: {
     AdminRestaurantForm,
   },
   methods: {
-    handleAfterSubmit(formData) {
-    for (let [name, value] of formData.entries()) {
-      console.log(name + ': ' + value)
+    async handleAfterSubmit(formData) {
+      try {
+        // STEP 3: 透過 restaurants.create 方法來向伺服器建立餐廳
+        const { data } = await adminAPI.restaurants.create({
+          formData,
+        });
+
+        if (data.status !== "success") {
+          throw new Error(data.message);
+        }
+
+        // STEP 4: 成功的話則轉址到 `/admin/restaurants`
+        this.$router.push({ name: "admin-restaurants" });
+      } catch (error) {
+        // STEP 5: 錯誤處理
+        Toast.fire({
+          icon: "error",
+          title: "無法建立餐廳，請稍後再試",
+        });
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
